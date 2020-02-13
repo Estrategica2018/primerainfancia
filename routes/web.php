f1b13ef8edb53cd99d40e96096c45a8f87d1d630
cristian <cristianjojoa01@gmail.com>
2020-02-13 13:21:31 -0500

    middleware rutas

--- routes/web.php
+++ routes/web.php
@@ -16,46 +16,40 @@ Route::get('/', function () {
 });*/
 
 Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home');
-
-Route::get('/comite', 'ComiteController@index')->name('comite');
-Route::get('/priorizacion', 'PriorizacionController@index')->name('priorizacion');
-Route::get('/priorizacion_resultado', 'PriorizacionController@priorizacion_resultado')->name('priorizacion_resultado');
-Route::get('/administrador', 'AdministradorController@index')->name('administrador');
+Route::group(['middleware' => ['auth']],function (){
 
 
-Route::get('/', 'HomeController@index')->name('home');
-Route::get('libros_dt', 'HomeController@libros_dt')->name('libros_dt');
-Route::post('registrar_libros_para_comite', 'HomeController@registrar_libros_para_comite')->name('registrar_libros_para_comite');
-Route::get('libros_preseleccion_dt', 'ComiteController@libros_preseleccion_dt')->name('libros_preseleccion_dt');
-Route::post('registrar_cupos_edades_priorizacion', 'HomeController@registrar_cupos_edades_priorizacion')->name('registrar_cupos_edades_priorizacion');
-Route::post('registrar_libros_para_priorizacion', 'HomeController@registrar_libros_para_priorizacion')->name('registrar_libros_para_priorizacion');
+    Route::get('/administrador', 'AdministradorController@index')->name('administrador')->middleware('auth', 'role:administrador_plataforma');
+    Route::get('usuarios_dt', 'AdministradorController@usuarios_dt')->name('usuarios_dt');
+    Route::post('registrar_usuarios', 'AdministradorController@registrar_usuarios')->name('registrar_usuarios');
+    Route::post('reiniciar_tablas', 'AdministradorController@reiniciar_tablas')->name('reiniciar_tablas')->middleware('auth', 'role:administrador_plataforma');
+    Route::post('editar_usuario', 'AdministradorController@editar_usuario')->name('editar_usuario');
+    Route::get('/administrador_priorizacion', 'AdministradorController@index_priorizacion')->name('administrador_priorizacion')->middleware('auth', 'role:administrador_plataforma');;
+    Route::post('registrar_libros_administrador_para_priorizacion', 'AdministradorController@registrar_libros_administrador_para_priorizacion')->name('registrar_libros_administrador_para_priorizacion');
+    Route::get('/historial_libros_usuario_dt/{userId?}/{tipoRegistro?}', 'AdministradorController@historial_libros_usuario_dt')->name('historial_libros_usuario_dt');
 
-Route::get('libros_priorizacion_dt', 'ComiteController@libros_priorizacion_dt')->name('libros_priorizacion_dt');
-Route::get('consultar_cupos_priorizacion/{tipo}', 'PriorizacionController@consultar_cupos_priorizacion')->name('consultar_cupos_priorizacion');
-
-Route::post('registrar_calificacion_priorizacion', 'PriorizacionController@registrar_calificacion_priorizacion')->name('registrar_calificacion_priorizacion');
-
-Route::get('resultado_libros_logistica_priorizacion_dt', 'PriorizacionController@resultado_libros_logistica_priorizacion_dt')->name('resultado_libros_logistica_priorizacion_dt');
-Route::get('libros_preseleccion_usuarios_dt/{usuario?}', 'ComiteController@libros_preseleccion_usuarios_dt')->name('libros_preseleccion_usuarios_dt');
-
-Route::get('usuarios_dt', 'AdministradorController@usuarios_dt')->name('usuarios_dt');
-
-Route::post('registrar_usuarios', 'AdministradorController@registrar_usuarios')->name('registrar_usuarios');
-
-Route::post('registrar_cupos_genero_priorizacion', 'HomeController@registrar_cupos_genero_priorizacion')->name('registrar_cupos_genero_priorizacion');
-
-Route::post('reiniciar_tablas', 'AdministradorController@reiniciar_tablas')->name('reiniciar_tablas');
-
-Route::post('editar_usuario', 'AdministradorController@editar_usuario')->name('editar_usuario');
-
-Route::get('/administrador_priorizacion', 'AdministradorController@index_priorizacion')->name('administrador_priorizacion');
-
-Route::post('registrar_libros_administrador_para_priorizacion', 'AdministradorController@registrar_libros_administrador_para_priorizacion')->name('registrar_libros_administrador_para_priorizacion');
-
-Route::post('elminar_libro_preseleccion', 'HomeController@elminar_libro_preseleccion')->name('elminar_libro_preseleccion');
+    
+    Route::get('/home', 'HomeController@index')->name('home');
+    Route::get('/', 'HomeController@index')->name('home');
+    Route::get('libros_dt', 'HomeController@libros_dt')->name('libros_dt');
+    Route::post('registrar_libros_para_comite', 'HomeController@registrar_libros_para_comite')->name('registrar_libros_para_comite');
+    Route::post('registrar_cupos_edades_priorizacion', 'HomeController@registrar_cupos_edades_priorizacion')->name('registrar_cupos_edades_priorizacion');
+    Route::post('registrar_libros_para_priorizacion', 'HomeController@registrar_libros_para_priorizacion')->name('registrar_libros_para_priorizacion');
+    Route::post('registrar_cupos_genero_priorizacion', 'HomeController@registrar_cupos_genero_priorizacion')->name('registrar_cupos_genero_priorizacion');
+    Route::post('elminar_libro_preseleccion', 'HomeController@elminar_libro_preseleccion')->name('elminar_libro_preseleccion');
 
 
-Route::get('/historial_libros_usuario_dt/{userId?}/{tipoRegistro?}', 'AdministradorController@historial_libros_usuario_dt')->name('historial_libros_usuario_dt');
+    Route::get('/comite', 'ComiteController@index')->name('comite')->middleware('auth', 'role:administrador_plataforma,comite_educativo');
+    Route::get('libros_preseleccion_dt', 'ComiteController@libros_preseleccion_dt')->name('libros_preseleccion_dt');
+    Route::get('libros_priorizacion_dt', 'ComiteController@libros_priorizacion_dt')->name('libros_priorizacion_dt');
+    Route::get('libros_preseleccion_usuarios_dt/{usuario?}', 'ComiteController@libros_preseleccion_usuarios_dt')->name('libros_preseleccion_usuarios_dt');
 
+
+    Route::get('/priorizacion', 'PriorizacionController@index')->name('priorizacion')->middleware('auth', 'role:administrador_plataforma,usuario');
+    Route::get('/priorizacion_resultado', 'PriorizacionController@priorizacion_resultado')->name('priorizacion_resultado')->middleware('auth', 'role:administrador_plataforma');
+    Route::get('consultar_cupos_priorizacion/{tipo}', 'PriorizacionController@consultar_cupos_priorizacion')->name('consultar_cupos_priorizacion');
+    Route::post('registrar_calificacion_priorizacion', 'PriorizacionController@registrar_calificacion_priorizacion')->name('registrar_calificacion_priorizacion');
+    Route::get('resultado_libros_logistica_priorizacion_dt', 'PriorizacionController@resultado_libros_logistica_priorizacion_dt')->name('resultado_libros_logistica_priorizacion_dt');
+
+
+});
